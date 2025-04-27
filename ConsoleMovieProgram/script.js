ed3aca1bd441569075246e191a6698b5a15b1064
Finn-Lunas <cenitelllll@gmail.com>
2025-04-27 21:29:10 +0300

    Виконав завдання 2: Додав декiлька умов та зробив валiдацiю введення фiльмiв та їх рейтингу

--- ConsoleMovieProgram/script.js
+++ ConsoleMovieProgram/script.js
@@ -10,10 +10,27 @@ const personalMovieDB = {
   privat: false,
 };
 
+if (personalMovieDB.count < 10 && personalMovieDB.count >= 0) {
+  alert("Ви переглянули доволі мало фільмів");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert("Ви класичний глядач");
+} else if (personalMovieDB.count > 30) {
+  alert("Ви кіноман");
+} else {
+  alert("Відбулася помилка");
+}
+
 for (let i = 0; i < 2; i++) {
   const movie = prompt("Один з останніх фільмів, який ви переглянули?");
   const rating = prompt("На скільки його оціните?");
-  personalMovieDB.movies[movie] = rating;
+  movie === "" ||
+  movie === null ||
+  rating === null ||
+  rating === "" ||
+  isNaN(rating) ||
+  movie.length >= 50
+    ? i--
+    : (personalMovieDB.movies[movie] = rating);
 }
 
 console.log(personalMovieDB);
